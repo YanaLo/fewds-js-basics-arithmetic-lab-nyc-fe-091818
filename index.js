e4dbc5148ccdc7ef2c7316329c2d6dac59042ac1
yana.loginova <yana.loginova@cix.csi.cuny.edu>
2019-03-05 20:24:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ newID+=oldID;
 
 let ageIsValid=Number.isInteger(currentAge)
 
-let randomNumber= Math.random()*20;
+let randomNumber= Math.random()*30;
 
 let randomInteger= Math. floor(randomNumber);
 
